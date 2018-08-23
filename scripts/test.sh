7fce12833f122d43b425aa5f5f94605a167325b9
anyric <anyamaronyango@gmail.com>
2018-08-23 19:36:47 +0300

    chore(docker): Refactor code
    - add setup.py file
    - update the docker-compose file
    - update the test.sh file

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 . /appenv/bin/activate
 
+# Download dependencies to a build cache
+pip download -d /build -r requirements_test.txt --no-input
+
 # Install application test requirements
-pip install -r requirements_test.txt
+pip install --no-index -f /build -r requirements_test.txt
 
 # Run test.sh arguments
 exec $@
