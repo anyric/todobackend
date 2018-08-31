50f689684db5cfc5c61867741721ac95d41f6d42
anyric <anyamaronyango@gmail.com>
2018-08-31 11:51:07 +0300

    add install docker-compose

--- packer/setup.sh
+++ packer/setup.sh
@@ -16,6 +16,9 @@ install_docker() {
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   sudo apt-get update -y
   sudo apt-get install -y git docker-ce build-essential
+  sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+
 }
 
 setup_application() {
