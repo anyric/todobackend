f7e9d72aff92ce001bab0fd19d61413018539190
anyric <anyamaronyango@gmail.com>
2018-08-31 13:15:13 +0300

    create docker user group and add current user to it

--- packer/setup.sh
+++ packer/setup.sh
@@ -16,16 +16,21 @@ install_docker() {
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   sudo apt-get update -y
   sudo apt-get install -y git docker-ce build-essential
+  apt-cache policy docker-engine
   sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
+
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+  sudo chmod g+rwx "/home/$USER/.docker" -R
+  sudo systemctl enable docker
   sudo systemctl start docker
 }
 
 setup_application() {
   git clone -b master https://github.com/anyric/todobackend.git
   cd /home/todo/todobackend
-  export DOCKER_HOST="tcp://localhost:4243"
-  sudo systemctl restart docker
   make test
   make build
   make release
