9925bb40978a0c1c098c726976b31e1b72e1c17e
anyric <anyamaronyango@gmail.com>
2018-08-31 11:09:52 +0300

    remove docker test command

--- packer/setup.sh
+++ packer/setup.sh
@@ -15,13 +15,12 @@ install_docker() {
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
   sudo apt-get update -y
-  sudo apt-get install -y docker-ce
-  # verify installation
-  sudo docker run hello-world
+  sudo apt-get install -y git docker-ce
 }
 
 setup_application() {
-  cd /home/todo/todobackend
+  echo "This is were I am"
+  pwd
   make test
   make build
   make release
