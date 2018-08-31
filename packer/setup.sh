c937a448ff5d0507aa69f81afefc2b0dd590d208
anyric <anyamaronyango@gmail.com>
2018-08-31 13:19:12 +0300

    fix docker user group conflict

--- packer/setup.sh
+++ packer/setup.sh
@@ -20,7 +20,6 @@ install_docker() {
   sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 
-  sudo groupadd docker
   sudo usermod -aG docker $USER
   sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
   sudo chmod g+rwx "/home/$USER/.docker" -R
