baed18324530d2deef08317c3b23935fd3af735b
anyric <anyamaronyango@gmail.com>
2018-08-31 14:14:30 +0300

    add debug message

--- packer/setup.sh
+++ packer/setup.sh
@@ -20,15 +20,17 @@ install_docker() {
   sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 
-  sudo usermod -aG docker $USER
+  sudo usermod -aG docker ${USER}
   export DOCKER_HOST="unix:///var/run/docker.sock"
   sudo systemctl enable docker
   sudo systemctl restart docker
+  sudo systemctl status docker
 }
 
 setup_application() {
   git clone -b master https://github.com/anyric/todobackend.git
   cd /home/todo/todobackend
+  docker info
   make test
   make build
   make release
